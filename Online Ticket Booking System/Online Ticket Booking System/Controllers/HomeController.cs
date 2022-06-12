2acbe486e011a1a5db716a9a4aabadc3c0948a2b
TATinev18 <56866283+TATinev18@users.noreply.github.com>
2022-06-13 00:10:13 +0300

    Make Contact Form To Send Emails

--- Online Ticket Booking System/Online Ticket Booking System/Controllers/HomeController.cs
+++ Online Ticket Booking System/Online Ticket Booking System/Controllers/HomeController.cs
@@ -5,6 +5,8 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Net;
+using System.Net.Mail;
 using System.Threading.Tasks;
 
 namespace Online_Ticket_Booking_System.Controllers
@@ -37,15 +39,72 @@ namespace Online_Ticket_Booking_System.Controllers
         {
             return View();
         }
-        public IActionResult ContactUs()
-        {
-            return View();
-        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        [HttpGet]
+        public IActionResult ContactUs()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult ContactUs(SendMailDto sendMailDto)
+        {
+            if (!ModelState.IsValid) return View();
+
+            try
+            {
+                MailMessage mail = new MailMessage();
+                // you need to enter your mail address
+                mail.From = new MailAddress("TATinev18@codingburgas.bg");
+
+                //To Email Address - your need to enter your to email address
+                mail.To.Add("TATinev18@codingburgas.bg");
+
+                mail.Subject = sendMailDto.Subject;
+
+                //you can specify also CC and BCC - i will skip this
+                //mail.CC.Add("");
+                //mail.Bcc.Add("");
+
+                mail.IsBodyHtml = true;
+
+                string content = "Name : " + sendMailDto.Name;
+                content += "<br/> Message : " + sendMailDto.Message;
+
+                mail.Body = content;
+
+
+                //create SMTP instant
+
+                //you need to pass mail server address and you can also specify the port number if you required
+                SmtpClient smtpClient = new SmtpClient("smtp.office365.com");
+
+                //Create nerwork credential and you need to give from email address and password
+                NetworkCredential networkCredential = new NetworkCredential("TATinev18@codingburgas.bg", "Sax23753");
+                smtpClient.UseDefaultCredentials = false;
+                smtpClient.Credentials = networkCredential;
+                smtpClient.Port = 25; // this is default port number - you can also change this
+                smtpClient.EnableSsl = true; // if ssl required you need to enable it
+                smtpClient.Send(mail);
+
+                ViewBag.Message = "Mail Send";
+
+                // now i need to create the from 
+                ModelState.Clear();
+
+            }
+            catch (Exception ex)
+            {
+                //If any error occured it will show
+                ViewBag.Message = ex.Message.ToString();
+            }
+            return View();
         }
     }
 }
